c4d2c37f23f4a067c9f3f85d95e0860a45279a9e
mmtakeuchi <mikemtaki@gmail.com>
2020-06-17 13:32:47 -0700

    created files

--- lib/flat_cocktails.rb
+++ lib/flat_cocktails.rb
@@ -1,4 +1,12 @@
-require "flat_cocktails/version"
+require "pry"
+require "rest-client"
+require "json"
+
+require_relative "./flat_cocktails/version"
+require_relative "./flat_cocktails/api.rb"
+require_relative "./flat_cocktails/cli.rb"
+require_relative "./flat_cocktails/cocktails.rb"
+
 
 module FlatCocktails
   class Error < StandardError; end
